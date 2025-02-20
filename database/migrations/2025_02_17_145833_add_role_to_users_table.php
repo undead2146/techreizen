abb85cf31432205178661b29ce0d7e593d750189
undead2146 <mustafa2146@live.be>
2025-02-20 11:03:24 +0100

    -a

--- database/migrations/2025_02_17_145833_add_role_to_users_table.php
+++ database/migrations/2025_02_17_145833_add_role_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->nullable();
+            $table->string('role');
         });
     }
 
