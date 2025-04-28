990bb58480efb44b1710d570c4c514401814a025
IDavidG <145495942+IDavidGI@users.noreply.github.com>
2025-04-28 11:57:23 +0200

    added small route changes to homepage

--- resources/views/traveller/home.blade.php
+++ resources/views/traveller/home.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class = "row justify-content-center">
+        <div class="text-center">
+            <div id="images" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                <!-- slideshow images -->
+                <div class="carousel-inner mx-auto">
+                    <div class="carousel-item active">
+                        <img src="{{ asset('images/header1.jpg') }}" class="img-fluid " alt="header 1">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('images/header2.jpg') }}" class="img-fluid " alt="header 2">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('images/header3.jpg') }}" class="img-fluid " alt="header 2">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
