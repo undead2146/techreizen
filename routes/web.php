b5970310822261223b7c1baf6abc269746ddd937
IDavidG <145495942+IDavidGI@users.noreply.github.com>
2025-05-08 14:24:38 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -95,9 +95,10 @@ Route::middleware(['auth', 'user-access:guide'])->group(function () {
     Route::get('/guide/home', [HomeController::class, 'guideHome'])->name('guide.home');
     
     // Guide-specific group management routes
-    Route::get('/guide/groups', [GroupController::class, 'guideIndex'])->name('guide.groups.index');
+    Route::get('/guide/groups', [GroupController::class, 'index'])->name('guide.groups.index');
     Route::get('/guide/groups/create', [GroupController::class, 'create'])->name('groups.create');
     Route::post('/guide/groups', [GroupController::class, 'store'])->name('groups.store');
+    Route::get('/guide/groups/{group}', [GroupController::class, 'show'])->name('guide.groups.show');
     Route::get('/guide/groups/{group}/edit', [GroupController::class, 'edit'])->name('groups.edit');
     Route::put('/guide/groups/{group}', [GroupController::class, 'update'])->name('groups.update');
     Route::delete('/guide/groups/{group}', [GroupController::class, 'destroy'])->name('groups.destroy');
